0465d6db6dcd2ce540af9775c5ad4eb6d49d39c0
MMikolaj <mikolajmazurkiewicz.90@gmail.com>
2022-02-08 13:52:02 +0100

    Update map.js
    back

--- scripts/map.js
+++ scripts/map.js
@@ -924,7 +924,8 @@ $(window).on('load', function() {
 
   /**
    * Loads the basemap and adds it to the map
-
+*/
+  
   function addBaseMap() {
     var basemap = trySetting('_tileProvider', 'CartoDB.Positron');
     L.tileLayer.provider(basemap, {
@@ -934,7 +935,6 @@ $(window).on('load', function() {
       position: trySetting('_mapAttribution', 'bottomright')
     }).addTo(map);
   }
-*/
   
   /**
    * Returns the value of a setting s
